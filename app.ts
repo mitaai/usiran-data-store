9356e324f9440491698eb65b509e8b8a43c47b3e
Ben Silverman <bsilverm@mit.edu>
2020-07-17 18:17:41 -0400

    Initialize data store

---
+++ app.ts
@@ -0,0 +1,285 @@
+import { use, schema } from 'nexus';
+import { prisma } from 'nexus-plugin-prisma';
+
+use(prisma({ 
+  migrations: false,
+  client: { options: { log: ['query'] } },
+  features: {
+    crud: true,
+  }
+}))
+
+// User
+
+schema.objectType({
+  name: 'User',
+  definition(t) {
+    t.model.createdAt()
+    t.model.email()
+    t.model.firstName()
+    t.model.id()
+    t.model.lastName()
+    t.model.role()
+    t.model.updatedAt()
+    t.model.userName()
+  },
+})
+
+// Document
+
+schema.objectType({
+  name: 'Document',
+  definition(t) {
+    t.model.id()
+    t.model.documentTitle()
+    t.model.documentCreationDate()
+    t.model.documentKind()
+    t.model.documentTags()
+    t.model.mentionedStakeholders()
+    t.model.documentAuthors()
+    t.model.documentDescription()
+    t.model.documentFiles()
+    t.model.mentionedLocations()
+    t.model.documentOriginalID()
+    t.model.documentClassification()
+    t.model.documentPublicationDate()
+    t.model.documentTranscript()
+  },
+})
+
+// Event
+
+schema.objectType({
+  name: 'Event',
+  definition(t) {
+    t.model.id()
+    t.model.eventTitle()
+    t.model.eventStartDate()
+    t.model.eventTags()
+    t.model.eventStakeholders()
+    t.model.eventDescription()
+    t.model.eventLocations()
+  },
+})
+
+// Stakeholder
+
+schema.objectType({
+  name: 'Stakeholder',
+  definition(t) {
+    t.model.id()
+    t.model.stakeholderFullName()
+    t.model.stakeholderDescription()
+    t.model.documents()
+    t.model.documentsMentionedIn()
+    t.model.eventsInvolvedIn()
+    t.model.stakeholderWikipediaUri()
+  },
+})
+
+// Location
+
+schema.objectType({
+  name: 'Location',
+  definition(t) {
+    t.model.id()
+    t.model.locationName()
+    t.model.locationDescription()
+    t.model.documentsMentionedIn()
+    t.model.locationEvents()
+    t.model.locationWikipediaUri()
+  },
+})
+
+// File
+
+schema.objectType({
+  name: 'File',
+  definition(t) {
+    t.model.id()
+    t.model.url()
+  },
+})
+
+// Relation tables
+
+schema.objectType({
+  name: 'KindOnDocument',
+  definition(t) {
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'TagOnDocument',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'BriefingBookDocument',
+  definition(t) {
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'BriefingBookEvent',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'ClassificationOnDocument',
+  definition(t) {
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'DocumentAuthor',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'DocumentEvent',
+  definition(t) {
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'DocumentFile',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'DocumentInvolvedStakeholder',
+  definition(t) {
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'DocumentLocation',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'LocationOnEvent',
+  definition(t) {
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'StakeholderBriefingBook',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'StakeholderEvent',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+schema.objectType({
+  name: 'TagOnEvent',
+  definition(t){
+    t.model.id()
+    t.model.A()
+    t.model.B()
+  }
+})
+
+// Query
+
+schema.queryType({
+  definition(t) {
+    t.crud.user()
+    t.crud.users({
+      pagination: true,
+      filtering: true,
+      ordering: true,
+    })
+    t.crud.document()
+    t.crud.documents({
+      pagination: true,
+      filtering: true,
+      ordering: true,
+    })
+    t.crud.event()
+    t.crud.events({
+      pagination: true,
+      filtering: true,
+      ordering: true,
+    })
+    t.crud.stakeholder()
+    t.crud.stakeholders({
+      pagination: true,
+      filtering: {
+        id: true,
+      },
+      ordering: true,
+    })
+    t.crud.location()
+    t.crud.locations({
+      pagination: true,
+      filtering: true,
+      ordering: true,
+    })
+    t.crud.file()
+    t.crud.files({
+      pagination: true,
+      filtering: true,
+      ordering: true,
+    })
+  },
+})
+
+// Mutation
+
+schema.mutationType({
+  definition(t) {
+    t.crud.createOneUser()
+    t.crud.updateOneUser()
+    t.crud.deleteOneUser()
+  },
+})
