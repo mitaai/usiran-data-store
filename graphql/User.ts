14c4da2ec52ac6b1d7ee247f6dc825271cf52b80
Ben Silverman <bsilverm@mit.edu>
2021-01-11 15:12:05 -0500

    #6 Adjust log settings

--- graphql/User.ts
+++ graphql/User.ts
@@ -15,9 +15,9 @@ const isJWTData = (input: object | string): input is JWTData => { return typeof 
 
 function getUserId(context: Context) {
   const Authorization = context.req.get('Authorization')
-  context.log.info('get user id');
+  context.log.debug('get user id');
   if (Authorization) {
-    context.log.info('authorization found');
+    context.log.debug('authorization found');
     const token = Authorization.replace('Bearer ', '')
     const verified = verify(token, process.env.AUTH_SECRET as Secret)
     if(isJWTData(verified)) {
--- util/logger.ts
+++ util/logger.ts
@@ -1,4 +1,4 @@
 import * as Floggy from 'floggy'
 
 export type Logger = Floggy.Logger;
-export const log = Floggy.log.child('app')
+export const log = Floggy.log;
