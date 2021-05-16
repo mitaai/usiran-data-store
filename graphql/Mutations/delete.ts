46ac5735ee70dd7f95b32716438603b41dbfe086
Ben Silverman <bsilverm@mit.edu>
2021-05-16 09:56:13 -0400

    Use tagged template $queryRaw, not function
    Closes #11.

--- graphql/Mutations/delete.ts
+++ graphql/Mutations/delete.ts
@@ -10,15 +10,15 @@ export const deleteMutations  = extendType({
     })
     t.crud.deleteOneDocument({
       async resolve(root, args, ctx, info, originalResolve) {
-        await ctx.db.$queryRaw(`DELETE FROM "BriefingBookDocument" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "ClassificationOnDocument" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentAuthor" WHERE "A" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentEvent" WHERE "A" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentFile" WHERE "A" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentInvolvedStakeholder" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentLocation" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "KindOnDocument" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "TagOnDocument" WHERE "B" = '${args.where.id}';`)
+        await ctx.db.$queryRaw`DELETE FROM "BriefingBookDocument" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "ClassificationOnDocument" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "DocumentAuthor" WHERE "A" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "DocumentEvent" WHERE "A" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "DocumentFile" WHERE "A" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "DocumentInvolvedStakeholder" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "DocumentLocation" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "KindOnDocument" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "TagOnDocument" WHERE "B" = '${args.where.id}';`
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
@@ -26,11 +26,11 @@ export const deleteMutations  = extendType({
     })
     t.crud.deleteOneEvent({
       async resolve(root, args, ctx, info, originalResolve) {
-        await ctx.db.$queryRaw(`DELETE FROM "BriefingBookEvent" WHERE "A" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentEvent" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "LocationOnEvent" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "StakeholderEvent" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "TagOnEvent" WHERE "B" = '${args.where.id}';`)
+        await ctx.db.$queryRaw`DELETE FROM "BriefingBookEvent" WHERE "A" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "DocumentEvent" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "LocationOnEvent" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "StakeholderEvent" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "TagOnEvent" WHERE "B" = '${args.where.id}';`
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
@@ -38,8 +38,8 @@ export const deleteMutations  = extendType({
     })
     t.crud.deleteOneLocation({
       async resolve(root, args, ctx, info, originalResolve) {
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentLocation" WHERE "A" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "LocationOnEvent" WHERE "A" = '${args.where.id}';`)
+        await ctx.db.$queryRaw`DELETE FROM "DocumentLocation" WHERE "A" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "LocationOnEvent" WHERE "A" = '${args.where.id}';`
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
@@ -47,10 +47,10 @@ export const deleteMutations  = extendType({
     })
     t.crud.deleteOneStakeholder({
       async resolve(root, args, ctx, info, originalResolve) {
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentAuthor" WHERE "B" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "DocumentInvolvedStakeholder" WHERE "A" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "StakeholderBriefingBook" WHERE "A" = '${args.where.id}';`)
-        await ctx.db.$queryRaw(`DELETE FROM "StakeholderEvent" WHERE "A" = '${args.where.id}';`)
+        await ctx.db.$queryRaw`DELETE FROM "DocumentAuthor" WHERE "B" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "DocumentInvolvedStakeholder" WHERE "A" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "StakeholderBriefingBook" WHERE "A" = '${args.where.id}';`
+        await ctx.db.$queryRaw`DELETE FROM "StakeholderEvent" WHERE "A" = '${args.where.id}';`
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
