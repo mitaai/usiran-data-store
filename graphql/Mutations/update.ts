ad66c6cb29878e9b37f32532e25c35f809811960
Ben Silverman <bsilverm@mit.edu>
2021-05-16 12:00:18 -0400

    Remove extraneous quotes from $queryRaw calls
    Closes #11.

--- graphql/Mutations/update.ts
+++ graphql/Mutations/update.ts
@@ -10,12 +10,12 @@ export const updateMutations  = extendType({
     })
     t.crud.updateOneDocument({
       async resolve(root, args, ctx, info, originalResolve) {
-        if(args.data.documentKind) await ctx.db.$queryRaw`DELETE FROM "KindOnDocument" WHERE "B" = '${args.where.id}';`
-        if(args.data.documentClassification) await ctx.db.$queryRaw`DELETE FROM "ClassificationOnDocument" WHERE "B" = '${args.where.id}';`
-        if(args.data.documentTags) await ctx.db.$queryRaw`DELETE FROM "TagOnDocument" WHERE "B" = '${args.where.id}';`
-        if(args.data.documentAuthors) await ctx.db.$queryRaw`DELETE FROM "DocumentAuthor" WHERE "A" = '${args.where.id}';`
-        if(args.data.mentionedStakeholders) await ctx.db.$queryRaw`DELETE FROM "DocumentInvolvedStakeholder" WHERE "B" = '${args.where.id}';`
-        if(args.data.mentionedLocations) await ctx.db.$queryRaw`DELETE FROM "DocumentLocation" WHERE "B" = '${args.where.id}';`;
+        if(args.data.documentKind) await ctx.db.$queryRaw`DELETE FROM "KindOnDocument" WHERE "B" = ${args.where.id};`
+        if(args.data.documentClassification) await ctx.db.$queryRaw`DELETE FROM "ClassificationOnDocument" WHERE "B" = ${args.where.id};`
+        if(args.data.documentTags) await ctx.db.$queryRaw`DELETE FROM "TagOnDocument" WHERE "B" = ${args.where.id};`
+        if(args.data.documentAuthors) await ctx.db.$queryRaw`DELETE FROM "DocumentAuthor" WHERE "A" = ${args.where.id};`
+        if(args.data.mentionedStakeholders) await ctx.db.$queryRaw`DELETE FROM "DocumentInvolvedStakeholder" WHERE "B" = ${args.where.id};`
+        if(args.data.mentionedLocations) await ctx.db.$queryRaw`DELETE FROM "DocumentLocation" WHERE "B" = ${args.where.id};`;
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
@@ -23,9 +23,9 @@ export const updateMutations  = extendType({
     })
     t.crud.updateOneEvent({
       async resolve(root, args, ctx, info, originalResolve) {
-        if(args.data.eventTags) await ctx.db.$queryRaw`DELETE FROM "TagOnEvent" WHERE "B" = '${args.where.id}';`
-        if(args.data.eventStakeholders) await ctx.db.$queryRaw`DELETE FROM "StakeholderEvent" WHERE "B" = '${args.where.id}';`
-        if(args.data.eventLocations) await ctx.db.$queryRaw`DELETE FROM "LocationOnEvent" WHERE "B" = '${args.where.id}';`
+        if(args.data.eventTags) await ctx.db.$queryRaw`DELETE FROM "TagOnEvent" WHERE "B" = ${args.where.id};`
+        if(args.data.eventStakeholders) await ctx.db.$queryRaw`DELETE FROM "StakeholderEvent" WHERE "B" = ${args.where.id};`
+        if(args.data.eventLocations) await ctx.db.$queryRaw`DELETE FROM "LocationOnEvent" WHERE "B" = ${args.where.id};`
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
@@ -33,9 +33,9 @@ export const updateMutations  = extendType({
     })
     t.crud.updateOneStakeholder({
       async resolve(root, args, ctx, info, originalResolve) {
-        if(args.data.documentsMentionedIn) await ctx.db.$queryRaw`DELETE FROM "DocumentInvolvedStakeholder" WHERE "A" = '${args.where.id}';`
-        if(args.data.documents) await ctx.db.$queryRaw`DELETE FROM "DocumentAuthor" WHERE "B" = '${args.where.id}';`
-        if(args.data.eventsInvolvedIn) await ctx.db.$queryRaw`DELETE FROM "StakeholderEvent" WHERE "A" = '${args.where.id}';`
+        if(args.data.documentsMentionedIn) await ctx.db.$queryRaw`DELETE FROM "DocumentInvolvedStakeholder" WHERE "A" = ${args.where.id};`
+        if(args.data.documents) await ctx.db.$queryRaw`DELETE FROM "DocumentAuthor" WHERE "B" = ${args.where.id};`
+        if(args.data.eventsInvolvedIn) await ctx.db.$queryRaw`DELETE FROM "StakeholderEvent" WHERE "A" = ${args.where.id};`
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
@@ -43,8 +43,8 @@ export const updateMutations  = extendType({
     })
     t.crud.updateOneLocation({
       async resolve(root, args, ctx, info, originalResolve) {
-        if(args.data.documentsMentionedIn) await ctx.db.$queryRaw`DELETE FROM "DocumentLocation" WHERE "A" = '${args.where.id}';`
-        if(args.data.locationEvents) await ctx.db.$queryRaw`DELETE FROM "LocationOnEvent" WHERE "A" = '${args.where.id}';`
+        if(args.data.documentsMentionedIn) await ctx.db.$queryRaw`DELETE FROM "DocumentLocation" WHERE "A" = ${args.where.id};`
+        if(args.data.locationEvents) await ctx.db.$queryRaw`DELETE FROM "LocationOnEvent" WHERE "A" = ${args.where.id};`
         const res = await originalResolve(root, args, ctx, info)
         return res
       },
